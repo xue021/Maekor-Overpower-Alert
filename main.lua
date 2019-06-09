ac31c78275bfaaf45e298556b06aeae99ce1702e
xue021 <37210622+xue021@users.noreply.github.com>
2019-06-09 16:44:49 -0400

    functionality update
    fixed bug that wouldnt update alert frame if an existing alert was already active

--- main.lua
+++ main.lua
@@ -1,104 +1,94 @@
 
-local inTrigger = 0
+-- initialize alert frame and all subframes
+local function initAlert()
 
-local function triggerAlert()
-	inTrigger = inTrigger + 1
-	
-	
-	
-	local AlertFrame = CreateFrame("Frame", "AlertFrame1", UIParent)
+	--create base frame
+	AlertFrame = CreateFrame("Frame", "AlertFrame", UIParent)
 	AlertFrame:SetSize(50, 50)
 	
-	
+	-- set initial position on first log on
 	if POSX == nil or POSY == nil then
 		AlertFrame:SetPoint("CENTER",100, 0)
-		_, _, _, POSX, POSY = AlertFrame:GetPoint()
-		
-	else
+		_, _, _, POSX, POSY = AlertFrame:GetPoint()		
+	else -- if not first log in, load saved position from SavedVariables: POSX and POSY
 		AlertFrame:SetPoint("CENTER",POSX, POSY)
 	end
 	
-	
+	-- the base alert frame is just a black square which will work as a background (makes the cooldown timer effect look better too)
 	AlertFrame.texture = AlertFrame:CreateTexture()
 	AlertFrame.texture:SetAllPoints()
 	AlertFrame.texture:SetColorTexture(0.0, 0.0, 0.0, 1)
-	--AlertFrame.texture:SetTexture("Interface\\Icons\\ability_meleedamage")
 	
-	
-
-	local AlertFrameIcon = CreateFrame("StatusBar", nil, AlertFrame)
+	-- create overpower icon frame
+	AlertFrameIcon = CreateFrame("StatusBar", nil, AlertFrame)
 	AlertFrameIcon:SetSize(50, 50)
 	AlertFrameIcon:SetPoint("TOP", 0, 0)
 	AlertFrameIcon.texture = AlertFrameIcon:CreateTexture()
 	AlertFrameIcon.texture:SetAllPoints(true)	
 	AlertFrameIcon.texture:SetTexture("Interface\\Icons\\ability_meleedamage")
 
-	-- make move able
-	AlertFrame:SetMovable(true)
-	AlertFrame:EnableMouse(true)
-	AlertFrame:RegisterForDrag("LeftButton")
-	AlertFrame:SetScript("OnDragStart", AlertFrame.StartMoving)	
-
-	function setFramePos()
-		AlertFrame:StopMovingOrSizing()
-		_, _, _, POSX, POSY = AlertFrame:GetPoint()
-		
-	end
-	AlertFrame:SetScript("OnDragStop",setFramePos)
-	
-	local AlertFrameFade = CreateFrame("StatusBar", nil, AlertFrameIcon)
+	-- this is the frame used to create the cooldown swipe / fade out animation
+	AlertFrameFade = CreateFrame("StatusBar", nil, AlertFrameIcon)
 	AlertFrameFade:SetSize(50, 50)
 	AlertFrameFade:SetPoint("TOP", 0, 0)
 	AlertFrameFade.texture = AlertFrameFade:CreateTexture()
 	AlertFrameFade.texture:SetAllPoints(true)	
 	AlertFrameFade.texture:SetColorTexture(0.0, 0.0, 0.0, 0.5)
-	
-	
 
-
-	local timerText = AlertFrame:CreateFontString(nil, "OVERLAY", "GameFontNormal")
+	-- this is the text that shows the remaning time on the current overpower window
+	timerText = AlertFrame:CreateFontString(nil, "OVERLAY", "GameFontNormal")
 	timerText:SetPoint("CENTER",0,-25-5)
 	timerText:SetText("")
 
+
+	-- makes the alert frame moveable 
+	AlertFrame:SetMovable(true)
+	AlertFrame:EnableMouse(true)
+	AlertFrame:RegisterForDrag("LeftButton")
+	AlertFrame:SetScript("OnDragStart", AlertFrame.StartMoving)	
+	function setFramePos()
+		AlertFrame:StopMovingOrSizing()
+		_, _, _, POSX, POSY = AlertFrame:GetPoint() -- saves points POSX and POSY to saved variables
+		
+	end
+	AlertFrame:SetScript("OnDragStop",setFramePos)
+
+	AlertFrame:Hide() -- hide the frame after done initializing
+
+end
+
+
+
+
+local function triggerAlert()
+	
+	-- show the frame
+	AlertFrame:Show()
 	
 	
-
-	-- timer and relevant time related updates to alert go here
-
-
-	
+	-- set a few useful variables
 	local START = 0
 	local END = 5
 	local timer = 0
 	AlertFrameFade:SetMinMaxValues(START, END)
+
+	--this is the script for the timer
 	AlertFrameFade:SetScript("OnUpdate", function(self, elapsed)
+		timer = timer + elapsed -- add the amount of time elapsed since last update to current timer
+		percDone = timer / END -- get percentage of total time elapsed 
+		AlertFrameFade:SetSize(50, 50*percDone) -- update the fade frame to reflect time remaining
+		timerText:SetText(string.format("%.1f", END - timer))--update the timer below the alert
 		
-		percDone = timer / END
-		AlertFrameFade:SetSize(50, 50*percDone)
-		timerText:SetText(string.format("%.1f", END - timer))
-		timer = timer + elapsed
-		if(inTrigger > 1) then
-			AlertFrame:Hide()
-			
-			timer = START
-				
-			
-			
-			inTrigger = inTrigger -1
-		end
+	
 		
-		-- when timer has reached the desired value, as defined by global END (seconds), restart it by setting it to 0, as defined by global START
+		-- when timer has reached the desired value, as defined by END (seconds), restart it by setting it to 0, as defined by START
 		if timer >= END then
-			timer = START
-			
-			AlertFrame:Hide()
-			inTrigger = inTrigger -1
+			timer = START -- reset timer to 0			
+			AlertFrame:Hide() -- hide the frame since completed
 			
 			
 		end
 	end)
-	--AlertFrame:Hide()
-	
 	
 end
 
@@ -127,16 +117,20 @@ local function OnEvent(self, event)
 	
 end
 
+-- create a /command to test the alert
 SLASH_MOA_TEST1 = "/moa"
 SlashCmdList["MOA_TEST"] = function(msg)
-   --print("Testing alert.")
    triggerAlert()  
 end 
 
 
 
-
+-- created hidden frame, register it to look at combat log events, on each combat log event load OnEvent() function
 local f = CreateFrame("Frame")
 f:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED", "player")
 f:SetScript("OnEvent", OnEvent)
+
+
 print("Maekor Overpower Alert loaded.")
+
+initAlert() -- initialize the alert frame
