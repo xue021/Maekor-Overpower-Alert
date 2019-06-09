a83d3a1ae68d2331629f4079b7b70011afc3d3fa
xue021 <37210622+xue021@users.noreply.github.com>
2019-06-09 00:09:14 -0400

    updated functionality

--- main.lua
+++ main.lua
@@ -1,41 +1,115 @@
 
-str = "DODGE"
-x = str:find("GE")
-
-print(x)
 
 
 local function triggerAlert()
-	local RedSquareTest = CreateFrame("Frame", nil, UIParent)
-	RedSquareTest:SetSize(50, 50)
-	RedSquareTest:SetPoint("CENTER", 0, 0)
-	RedSquareTest.texture = RedSquareTest:CreateTexture(nil, "BACKGROUND")
-	RedSquareTest.texture:SetAllPoints(true)
-	RedSquareTest.texture:SetColorTexture(1.0, 0.0, 0.0, 0.5)
+	if 	AlertFrame1 then 
+		print(AlertFrame1:IsVisible())
+
+		if AlertFrame1:IsVisible() then
+			print("hiding old frame")
+			AlertFrame1:Hide()
+			AlertFrame1 = nil
+		end
+		
+	end
 	
-	C_Timer.After(3, function()RedSquareTest:Hide() end )
+	
+	local AlertFrame = CreateFrame("Frame", "AlertFrame1", UIParent)
+	AlertFrame:SetSize(50, 50)
+	print(AlertFrame)
+	
+	if POSX == nil or POSY == nil then
+		AlertFrame:SetPoint("CENTER",100, 0)
+		_, _, _, POSX, POSY = AlertFrame:GetPoint()
+		
+	else
+		AlertFrame:SetPoint("CENTER",POSX, POSY)
+	end
+	
+	
+	AlertFrame.texture = AlertFrame:CreateTexture()
+	AlertFrame.texture:SetAllPoints()
+	AlertFrame.texture:SetColorTexture(0, 0.0, 0.0, 1)
+	--AlertFrame.texture:SetTexture("Interface\\Icons\\ability_meleedamage")
+	
+
+	local AlertFrameIcon = CreateFrame("StatusBar", nil, AlertFrame)
+	AlertFrameIcon:SetSize(50, 50)
+	AlertFrameIcon:SetPoint("TOP", 0, 0)
+	AlertFrameIcon.texture = AlertFrameIcon:CreateTexture()
+	AlertFrameIcon.texture:SetAllPoints(true)	
+	AlertFrameIcon.texture:SetTexture("Interface\\Icons\\ability_meleedamage")
+
+	-- make move able
+	AlertFrame:SetMovable(true)
+	AlertFrame:EnableMouse(true)
+	AlertFrame:RegisterForDrag("LeftButton")
+	AlertFrame:SetScript("OnDragStart", AlertFrame.StartMoving)	
+
+	function setFramePos()
+		AlertFrame:StopMovingOrSizing()
+		_, _, _, POSX, POSY = AlertFrame:GetPoint()
+		
+	end
+	AlertFrame:SetScript("OnDragStop",setFramePos)
+	
+
+	
+
+
+
+	local timerText = AlertFrame:CreateFontString(nil, "OVERLAY", "GameFontNormal")
+	timerText:SetPoint("CENTER",0,-25-5)
+	timerText:SetText("")
+
+	local AlertFrameFade = CreateFrame("StatusBar", nil, AlertFrame)
+	AlertFrameFade:SetSize(50, 50)
+	AlertFrameFade:SetPoint("TOP", 0, 0)
+	AlertFrameFade.texture = AlertFrameFade:CreateTexture()
+	AlertFrameFade.texture:SetAllPoints(true)	
+	AlertFrameFade.texture:SetColorTexture(0.8, 0.8, 0.8, 0.5)
+	
+
+	-- timer and relevant time related updates to alert go here
+
+
+	
+	local START = 0
+	local END = 5
+	local timer = 0
+	AlertFrameFade:SetMinMaxValues(START, END)
+	AlertFrameFade:SetScript("OnUpdate", function(self, elapsed)
+		
+		percDone = timer / END
+		AlertFrameFade:SetSize(50, 50*percDone)
+		timerText:SetText(string.format("%.1f", END - timer))
+		timer = timer + elapsed
+		
+		
+		-- when timer has reached the desired value, as defined by global END (seconds), restart it by setting it to 0, as defined by global START
+		if timer >= END then
+			timer = START
+			
+			AlertFrame:Hide()
+			
+			
+		end
+	end)
+	--AlertFrame:Hide()
+	
 	
 end
 
 
--- combat log addon
+-- combat log function
 local function OnEvent(self, event)
 	local eventSearchingFor = "DODGE" --or DODGE
 	arr = {}
 	arr[1], arr[2], arr[3], arr[4],arr[5],arr[6],arr[7],arr[8],arr[9],arr[10],arr[11],arr[12],arr[13],arr[14],arr[15],arr[16],arr[17],arr[18],arr[19],arr[20] = CombatLogGetCurrentEventInfo()
-	--local arg1, arg2, arg3, arg4,arg5,arg6,arg7,arg8,arg9,arg10,arg11,arg12,arg13,arg14,arg15,arg16,arg17,arg18,arg19,arg20 = CombatLogGetCurrentEventInfo()
-	
-	
-	
 	
 	
 	if arr[5] == UnitName("player") then
 	
-		
-	
-		
-		
-		
 		-- below works (on swings and spell)
 		
 		if arr[12]==eventSearchingFor or arr[15] == eventSearchingFor then
@@ -51,7 +125,16 @@ local function OnEvent(self, event)
 	
 end
 
+SLASH_MOA_TEST1 = "/moa"
+SlashCmdList["MOA_TEST"] = function(msg)
+   print("Testing alert.")
+   triggerAlert()  
+end 
+
+
+
+
 local f = CreateFrame("Frame")
 f:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED", "player")
 f:SetScript("OnEvent", OnEvent)
-print("hello")
+print("Maekor Overpower Alert loaded.")
